1f1015f0df932f9b0d352b1fc97e5478b33ea9b4
Pablo Curell <pablocm90@gmail.com>
2020-12-28 17:09:37 +0100

    [ADD] first test passed

--- rspec-course/spec/card_spec.rb
+++ rspec-course/spec/card_spec.rb
@@ -1,5 +1,15 @@
-Rspec.describe 'Card' do
+RSpec.describe 'Card' do
   it 'has a type' do
-    
+    card = Card.new('Ace of Spades')
+    expect(card.type).to eq('Ace of Spades')
+  end
+end
+
+
+class Card 
+  attr_reader :type
+  
+  def initialize(type)
+    @type = type
   end
 end
